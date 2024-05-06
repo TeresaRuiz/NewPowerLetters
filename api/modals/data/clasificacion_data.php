80194b7ecc3c44b3e956101eddac9f28a85b860c
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-06 08:22:22 -0600

    cambios  en data, php, html de clasificacion

--- api/modals/data/clasificacion_data.php
+++ api/modals/data/clasificacion_data.php
@@ -49,6 +49,31 @@ class clasificacionData extends clasificacionHandler
             return false;
         }
     }
+
+    public function setDescripcion($value, $min = 2, $max = 50)
+    {
+        // Verificar si la clasificación ya existe en la base de datos
+        $checkSql = 'SELECT COUNT(*) as count FROM tb_clasificaciones WHERE nombre = ?';
+        $checkParams = array($value);
+        $checkResult = Database::getRow($checkSql, $checkParams);
+    
+        if ($checkResult['count'] > 0) {
+            $this->data_error = 'La clasificación ya existe';
+            return false;
+        }
+    
+        // Validar el valor y la longitud del nombre de la clasificación
+        if (!Validator::validateAlphabetic($value)) {
+            $this->data_error = 'El nombre de la clasificación debe ser un valor alfabético';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre = $value;
+            return true;
+        } else {
+            $this->data_error = 'El nombre de la clasificación debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
     
 
 
