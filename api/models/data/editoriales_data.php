5a9f731b4e0be098d20d77287eb9a54b9df7b3fb
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-06 22:10:28 -0600

    cambios en data handler php y html de editoriales, añadiendo codigos

---
+++ api/models/data/editoriales_data.php
@@ -0,0 +1,53 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/genero_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
+class GeneroData extends GeneroHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null; // Variable para almacenar mensajes de error.
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        // Valida que el identificador sea un número natural.
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value; // Asigna el valor del identificador.
+            return true;
+        } else {
+            $this->data_error = 'El identificador del género es incorrecto'; // Almacena mensaje de error.
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        // Valida que el nombre sea alfanumérico.
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico'; // Almacena mensaje de error.
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre = $value; // Asigna el valor del nombre.
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max; // Almacena mensaje de error.
+            return false;
+        }
+    }
+
+    /*
+     *  Métodos para obtener los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error; // Devuelve el mensaje de error.
+    }
+}
---
+++ api/services/admin/editoriales.php
@@ -0,0 +1,97 @@
+<?php
+// Se incluye la clase del modelo.
+require_once('../../models/data/genero_data.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $genero = new generoData;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'fileStatus' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['idAdministrador']) or true) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'searchRows':
+                if (!Validator::validateSearch($_POST['search'])) {
+                    $result['error'] = Validator::getSearchError();
+                } elseif ($result['dataset'] = $genero->searchRows()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' coincidencias';
+                } else {
+                    $result['error'] = 'No hay coincidencias';
+                }
+                break;
+            case 'createRow':
+                $_POST = Validator::validateForm($_POST);
+                if (!$genero->setNombre($_POST['genero'])) {
+                    $result['error'] = $genero->getDataError();
+                } elseif ($genero->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Género creado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al crear el género';
+                }
+                break;
+            case 'readAll':
+                if ($result['dataset'] = $genero->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' registros';
+                } else {
+                    $result['error'] = 'No existen generos de zapatos registradas';
+                }
+                break;
+            case 'readOne':
+                if (!$genero->setId($_POST['idGenero'])) {
+                    $result['error'] = $genero->getDataError();
+                } elseif ($result['dataset'] = $genero->readOne()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'Genero de zapato inexistente';
+                }
+                break;
+            case 'updateRow':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$genero->setId($_POST['idGenero']) or
+                    !$genero->setFilename() or
+                    !$genero->setNombre($_POST['nombreGEN']) or
+                    !$genero->setImagen($_FILES['nombreIMG'], $categoria->getFilename())
+                ) {
+                    $result['error'] = $genero->getDataError();
+                } elseif ($genero->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Genero del zapato modificado correctamente';
+                    // Se asigna el estado del archivo después de actualizar.
+                    $result['fileStatus'] = Validator::changeFile($_FILES['nombreIMG'], $genero::RUTA_IMAGEN, $categoria->getFilename());
+                } else {
+                    $result['error'] = 'Ocurrió un problema al modificar el genero del zapato';
+                }
+                break;
+            case 'deleteRow':
+                if (!$genero->setId($_POST['idGenero'])) {
+                    $result['error'] = $genero->getDataError();
+                } elseif ($genero->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Genero del zapato eliminado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al eliminar el genero del zapato';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+        $result['exception'] = Database::getException();
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('Content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
