608ac8d8974cb84cfec840efcbf53c4f9c6abccb
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-07 13:40:03 -0600

    cambios nuevos en el handler de clasificaciones

--- api/models/handler/clasificacion_handler.php
+++ api/models/handler/clasificacion_handler.php
@@ -18,8 +18,8 @@ class clasificacionHandler
         $value = '%' . Validator::getSearchValue() . '%';
 
         // Definir la consulta SQL para buscar coincidencias en la tabla tb_generos
-        $sql = 'SELECT id_genero, nombre
-            FROM tb_generos
+        $sql = 'SELECT id_clasificacion, nombre, descripcion
+            FROM tb_clasificaciones
             WHERE nombre LIKE ?
             ORDER BY nombre'; // Ordenar por nombre para un resultado ordenado
 
@@ -34,8 +34,8 @@ class clasificacionHandler
     public function createRow()
     {
         // Cambiar el nombre de las columnas y sus valores
-        $sql = 'INSERT INTO tb_generos (nombre) VALUES (?)';
-        $params = array($this->nombre); // Solo necesitamos el nombre
+        $sql = 'INSERT INTO tb_clasificaciones (nombre, descripcion) VALUES (?,?)';
+        $params = array($this->nombre, $this->descripcion); // Solo necesitamos el nombre
         return Database::executeRow($sql, $params);
     }
 
@@ -43,7 +43,7 @@ class clasificacionHandler
     public function readAll()
     {
         // Cambiar las columnas seleccionadas y la tabla
-        $sql = 'SELECT id_genero, nombre FROM tb_generos ORDER BY nombre';
+        $sql = 'SELECT id_clasificacion, nombre, descripcion FROM tb_clasificaciones ORDER BY nombre';
         return Database::getRows($sql);
     }
 
@@ -51,7 +51,7 @@ class clasificacionHandler
     public function readOne()
     {
         // Buscar por id en tb_generos
-        $sql = 'SELECT id_genero, nombre FROM tb_generos WHERE id_genero = ?';
+        $sql = 'SELECT id_clasificacion, nombre, descripcion FROM tb_clasificaciones WHERE id_clasificacion = ?';
         $params = array($this->id); // Id para filtrar
         return Database::getRow($sql, $params);
     }
@@ -60,8 +60,8 @@ class clasificacionHandler
     public function updateRow()
     {
         // Cambiar la consulta SQL para actualizar el nombre
-        $sql = 'UPDATE tb_generos SET nombre = ? WHERE id_genero = ?';
-        $params = array($this->nombre, $this->id); // Parámetros de actualización
+        $sql = 'UPDATE tb_clasificaciones SET nombre = ?, descripcion= ? WHERE id_clasificacion = ?';
+        $params = array($this->nombre, $this->descripcion, $this->id); // Parámetros de actualización
         return Database::executeRow($sql, $params);
     }
 
@@ -69,7 +69,7 @@ class clasificacionHandler
     public function deleteRow()
     {
         // Cambiar la consulta SQL para eliminar por id
-        $sql = 'DELETE FROM tb_generos WHERE id_genero = ?';
+        $sql = 'DELETE FROM tb_clasificaciones WHERE id_clasificacion = ?';
         $params = array($this->id); // Parámetro para la eliminación
         return Database::executeRow($sql, $params);
     }
