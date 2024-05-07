dc750f16614978afa4a21ea0458a4bb0b08965b5
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-06 22:30:08 -0600

    cambios en data y js de editoriales

--- api/models/data/editoriales_data.php
+++ api/models/data/editoriales_data.php
@@ -2,11 +2,11 @@
 // Se incluye la clase para validar los datos de entrada.
 require_once('../../helpers/validator.php');
 // Se incluye la clase padre.
-require_once('../../models/handler/genero_handler.php');
+require_once('../../models/handler/editoriales_handler.php');
 /*
  *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
  */
-class GeneroData extends GeneroHandler
+class editorialData extends editorialesHandler
 {
     /*
      *  Atributos adicionales.
