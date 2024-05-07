5e793a722543f73413ce15bd732d40f405ee3d92
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-06 22:26:12 -0600

    cambos en el php editoriales

--- api/services/admin/editoriales.php
+++ api/services/admin/editoriales.php
@@ -1,13 +1,13 @@
 <?php
 // Se incluye la clase del modelo.
-require_once('../../models/data/genero_data.php');
+require_once('../../models/data/editoriales_data.php');
 
 // Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
 if (isset($_GET['action'])) {
     // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
     session_start();
     // Se instancia la clase correspondiente.
-    $genero = new generoData;
+    $editorial = new editorialData;
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'fileStatus' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
@@ -17,7 +17,7 @@ if (isset($_GET['action'])) {
             case 'searchRows':
                 if (!Validator::validateSearch($_POST['search'])) {
                     $result['error'] = Validator::getSearchError();
-                } elseif ($result['dataset'] = $genero->searchRows()) {
+                } elseif ($result['dataset'] = $editorial->searchRows()) {
                     $result['status'] = 1;
                     $result['message'] = 'Existen ' . count($result['dataset']) . ' coincidencias';
                 } else {
@@ -26,9 +26,9 @@ if (isset($_GET['action'])) {
                 break;
             case 'createRow':
                 $_POST = Validator::validateForm($_POST);
-                if (!$genero->setNombre($_POST['genero'])) {
-                    $result['error'] = $genero->getDataError();
-                } elseif ($genero->createRow()) {
+                if (!$editorial->setNombre($_POST['editorial'])) {
+                    $result['error'] = $editorial->getDataError();
+                } elseif ($editorial->createRow()) {
                     $result['status'] = 1;
                     $result['message'] = 'Género creado correctamente';
                 } else {
@@ -36,7 +36,7 @@ if (isset($_GET['action'])) {
                 }
                 break;
             case 'readAll':
-                if ($result['dataset'] = $genero->readAll()) {
+                if ($result['dataset'] = $editorial->readAll()) {
                     $result['status'] = 1;
                     $result['message'] = 'Existen ' . count($result['dataset']) . ' registros';
                 } else {
@@ -44,9 +44,9 @@ if (isset($_GET['action'])) {
                 }
                 break;
             case 'readOne':
-                if (!$genero->setId($_POST['idGenero'])) {
-                    $result['error'] = $genero->getDataError();
-                } elseif ($result['dataset'] = $genero->readOne()) {
+                if (!$editorial->setId($_POST['idEditorial'])) {
+                    $result['error'] = $editorial->getDataError();
+                } elseif ($result['dataset'] = $editorial->readOne()) {
                     $result['status'] = 1;
                 } else {
                     $result['error'] = 'Genero de zapato inexistente';
@@ -55,25 +55,22 @@ if (isset($_GET['action'])) {
             case 'updateRow':
                 $_POST = Validator::validateForm($_POST);
                 if (
-                    !$genero->setId($_POST['idGenero']) or
-                    !$genero->setFilename() or
-                    !$genero->setNombre($_POST['nombreGEN']) or
-                    !$genero->setImagen($_FILES['nombreIMG'], $categoria->getFilename())
+                    !$editorial->setId($_POST['idEditorial']) or
+                    !$editorial->setNombre($_POST['editorial'])
                 ) {
-                    $result['error'] = $genero->getDataError();
-                } elseif ($genero->updateRow()) {
+                    $result['error'] = $editorial->getDataError();
+                } elseif ($editorial->updateRow()) {
                     $result['status'] = 1;
                     $result['message'] = 'Genero del zapato modificado correctamente';
                     // Se asigna el estado del archivo después de actualizar.
-                    $result['fileStatus'] = Validator::changeFile($_FILES['nombreIMG'], $genero::RUTA_IMAGEN, $categoria->getFilename());
                 } else {
                     $result['error'] = 'Ocurrió un problema al modificar el genero del zapato';
                 }
                 break;
             case 'deleteRow':
-                if (!$genero->setId($_POST['idGenero'])) {
-                    $result['error'] = $genero->getDataError();
-                } elseif ($genero->deleteRow()) {
+                if (!$editorial->setId($_POST['idEditorial'])) {
+                    $result['error'] = $editorial->getDataError();
+                } elseif ($editorial->deleteRow()) {
                     $result['status'] = 1;
                     $result['message'] = 'Genero del zapato eliminado correctamente';
                 } else {
