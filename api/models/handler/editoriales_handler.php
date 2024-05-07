aead9a49e00bd840141dccee525c28c8bda8e558
JV26vasr <veronicaro3101qr@gmail.com>
2024-05-06 22:28:22 -0600

    cambios en el handler de editoriales

--- api/models/handler/editoriales_handler.php
+++ api/models/handler/editoriales_handler.php
@@ -4,7 +4,7 @@ require_once ('../../helpers/database.php');
 /*
  *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
  */
-class generoHandler
+class editorialesHandler
 {/*
  *  Declaración de atributos para el manejo de datos.
  */
@@ -17,8 +17,8 @@ class generoHandler
         $value = '%' . Validator::getSearchValue() . '%';
 
         // Definir la consulta SQL para buscar coincidencias en la tabla tb_generos
-        $sql = 'SELECT id_genero, nombre
-            FROM tb_generos
+        $sql = 'SELECT id_editorial, nombre
+            FROM tb_editoriales
             WHERE nombre LIKE ?
             ORDER BY nombre'; // Ordenar por nombre para un resultado ordenado
 
@@ -33,7 +33,7 @@ class generoHandler
     public function createRow()
     {
         // Cambiar el nombre de las columnas y sus valores
-        $sql = 'INSERT INTO tb_generos (nombre) VALUES (?)';
+        $sql = 'INSERT INTO tb_editoriales (nombre) VALUES (?)';
         $params = array($this->nombre); // Solo necesitamos el nombre
         return Database::executeRow($sql, $params);
     }
@@ -42,7 +42,7 @@ class generoHandler
     public function readAll()
     {
         // Cambiar las columnas seleccionadas y la tabla
-        $sql = 'SELECT id_genero, nombre FROM tb_generos ORDER BY nombre';
+        $sql = 'SELECT id_editorial, nombre FROM tb_editoriales ORDER BY nombre';
         return Database::getRows($sql);
     }
 
@@ -50,7 +50,7 @@ class generoHandler
     public function readOne()
     {
         // Buscar por id en tb_generos
-        $sql = 'SELECT id_genero, nombre FROM tb_generos WHERE id_genero = ?';
+        $sql = 'SELECT id_editorial, nombre FROM tb_editoriales WHERE id_editorial = ?';
         $params = array($this->id); // Id para filtrar
         return Database::getRow($sql, $params);
     }
@@ -59,7 +59,7 @@ class generoHandler
     public function updateRow()
     {
         // Cambiar la consulta SQL para actualizar el nombre
-        $sql = 'UPDATE tb_generos SET nombre = ? WHERE id_genero = ?';
+        $sql = 'UPDATE tb_editoriales SET nombre = ? WHERE id_editorial = ?';
         $params = array($this->nombre, $this->id); // Parámetros de actualización
         return Database::executeRow($sql, $params);
     }
@@ -68,7 +68,7 @@ class generoHandler
     public function deleteRow()
     {
         // Cambiar la consulta SQL para eliminar por id
-        $sql = 'DELETE FROM tb_generos WHERE id_genero = ?';
+        $sql = 'DELETE FROM tb_editoriales WHERE id_editorial = ?';
         $params = array($this->id); // Parámetro para la eliminación
         return Database::executeRow($sql, $params);
     }
