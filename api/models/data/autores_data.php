ccaeb0525ce9cf46b5b7d16eeee8f469c88ed6db
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-05-07 14:44:02 -0600

    cambios en php handler de autores

---
+++ api/models/data/autores_data.php
@@ -0,0 +1,68 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/clasificacion_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
+class clasificacionData extends clasificacionHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null; // Variable para almacenar mensajes de error.
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        // Valida que el identificador sea un número natural.
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value; // Asigna el valor del identificador.
+            return true;
+        } else {
+            $this->data_error = 'El identificador de la clasificación es incorrecto'; // Almacena mensaje de error.
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        // Valida que el nombre sea alfanumérico.
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico'; // Almacena mensaje de error.
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre = $value; // Asigna el valor del nombre.
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max; // Almacena mensaje de error.
+            return false;
+        }
+    }
+
+    public function setDescripcion($value, $min = 2, $max = 50)
+    {
+        // Valida que el nombre sea alfanumérico.
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'La descripción debe ser un valor alfanumérico'; // Almacena mensaje de error.
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->descripcion = $value; // Asigna el valor del nombre.
+            return true;
+        } else {
+            $this->data_error = 'La descripcion debe tener una longitud entre ' . $min . ' y ' . $max; // Almacena mensaje de error.
+            return false;
+        }
+    }
+
+    /*
+     *  Métodos para obtener los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error; // Devuelve el mensaje de error.
+    }
+}
---
+++ api/models/handler/autores_handler.php
@@ -0,0 +1,77 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once ('../../helpers/database.php');
+/*
+ *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
+ */
+class clasificacionHandler
+{/*
+ *  Declaración de atributos para el manejo de datos.
+ */
+    protected $id = null;
+    protected $nombre = null;
+    protected $descripcion = null;
+
+    public function searchRows()
+    {
+        // Obtener el valor de búsqueda y envolverlo con comodines para usar con LIKE
+        $value = '%' . Validator::getSearchValue() . '%';
+
+        // Definir la consulta SQL para buscar coincidencias en la tabla tb_generos
+        $sql = 'SELECT id_clasificacion, nombre, descripcion
+            FROM tb_clasificaciones
+            WHERE nombre LIKE ?
+            ORDER BY nombre'; // Ordenar por nombre para un resultado ordenado
+
+        // Establecer los parámetros para la consulta (el término de búsqueda)
+        $params = array($value);
+
+        // Ejecutar la consulta y devolver las filas resultantes
+        return Database::getRows($sql, $params);
+    }
+
+    // Para crear una nueva fila en tb_generos
+    public function createRow()
+    {
+        // Cambiar el nombre de las columnas y sus valores
+        $sql = 'INSERT INTO tb_clasificaciones (nombre, descripcion) VALUES (?,?)';
+        $params = array($this->nombre, $this->descripcion); // Solo necesitamos el nombre
+        return Database::executeRow($sql, $params);
+    }
+
+    // Para leer todas las filas de tb_generos
+    public function readAll()
+    {
+        // Cambiar las columnas seleccionadas y la tabla
+        $sql = 'SELECT id_clasificacion, nombre, descripcion FROM tb_clasificaciones ORDER BY nombre';
+        return Database::getRows($sql);
+    }
+
+    // Para leer una fila específica por id
+    public function readOne()
+    {
+        // Buscar por id en tb_generos
+        $sql = 'SELECT id_clasificacion, nombre, descripcion FROM tb_clasificaciones WHERE id_clasificacion = ?';
+        $params = array($this->id); // Id para filtrar
+        return Database::getRow($sql, $params);
+    }
+
+    // Para actualizar una fila específica por id
+    public function updateRow()
+    {
+        // Cambiar la consulta SQL para actualizar el nombre
+        $sql = 'UPDATE tb_clasificaciones SET nombre = ?, descripcion= ? WHERE id_clasificacion = ?';
+        $params = array($this->nombre, $this->descripcion, $this->id); // Parámetros de actualización
+        return Database::executeRow($sql, $params);
+    }
+
+    // Para eliminar una fila específica por id
+    public function deleteRow()
+    {
+        // Cambiar la consulta SQL para eliminar por id
+        $sql = 'DELETE FROM tb_clasificaciones WHERE id_clasificacion = ?';
+        $params = array($this->id); // Parámetro para la eliminación
+        return Database::executeRow($sql, $params);
+    }
+}
+;
