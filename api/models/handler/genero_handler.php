a0d09e7fc5562f837e176f9e2024a0f549ec5be1
Enrique2442 <107508345+TeresaRuiz@users.noreply.github.com>
2024-05-20 11:03:59 -0600

    Arreglando generos

--- api/models/handler/genero_handler.php
+++ api/models/handler/genero_handler.php
@@ -4,7 +4,7 @@ require_once ('../../helpers/database.php');
 /*
  *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
  */
-class generoHandler
+class GeneroHandler
 {/*
  *  Declaración de atributos para el manejo de datos.
  */
--- api/services/admin/genero.php
+++ api/services/admin/genero.php
@@ -8,7 +8,7 @@ if (isset($_GET['action'])) {
     session_start();
 
     // Crear una instancia de la clase 'generoData' para interactuar con los datos relacionados con 'género'.
-    $genero = new generoData;
+    $genero = new GeneroData;
 
     // Inicializar un arreglo para almacenar el resultado de las operaciones de la API.
     $result = array(
