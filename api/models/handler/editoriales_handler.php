5ee259192e33e750f109ebf866e15a62231edee8
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-05-20 09:42:49 -0600

    me estresaba ver el codigo demasiado junto

--- api/models/handler/editoriales_handler.php
+++ api/models/handler/editoriales_handler.php
@@ -1,11 +1,14 @@
 <?php
 // Se incluye la clase para trabajar con la base de datos.
+
 require_once ('../../helpers/database.php');
 /*
  *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
  */
+
 class editorialesHandler
-{/*
+{
+    /*
  *  Declaración de atributos para el manejo de datos.
  */
     protected $id = null;
