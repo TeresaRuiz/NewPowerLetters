f43a31d0c090c6ca0def32261d53a78ca87c217d
Teresa <20220651@ricaldone.edu.sv>
2024-05-20 21:41:18 -0600

    Ya sirve la autenticacion

--- resources/js/header_admin.js
+++ resources/js/header_admin.js
@@ -1,100 +1,106 @@
 // Constante para completar la ruta de la API.
 const USER_API = 'services/admin/administrador.php';
 
-// Crea el elemento header
-const header = document.createElement('header');
-header.classList.add('header');
-header.id = 'header';
+// Función asíncrona para cargar el encabezado y pie del documento.
+const loadTemplate = async () => {
+    try {
+        // Petición para obtener el nombre del usuario que ha iniciado sesión.
+        const DATA = await fetchData(USER_API, 'getUser');
+        // Se verifica si el usuario está autenticado, de lo contrario se envía a iniciar sesión.
+        if (DATA.session) {
+            // Se comprueba si existe un alias definido para el usuario, de lo contrario se muestra un mensaje con la excepción.
+            if (DATA.status) {
+                // Crear y agregar el encabezado de la página web antes del contenido principal.
+                const header = document.createElement('header');
+                header.classList.add('header');
+                header.innerHTML = `
+                    <nav class="nav container">
+                        <a href="#" class="nav__logo">
+                            <i class="ri-book-3-line"></i> Power Letters
+                        </a>
+                        <div class="nav__menu">
+                            <ul class="nav__list">
+                                <li class="nav__item">
+                                    <a href="../Private/inicio_admin.html" class="nav__link active-link">
+                                        <i class="ri-home-line"></i>
+                                        <span>Inicio</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item">
+                                    <a href="../Private/libros.html" class="nav__link">
+                                        <i class="ri-book-3-line"></i>
+                                        <span>Libros</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item">
+                                    <a href="../Private/generos.html" class="nav__link">
+                                        <i class="ri-book-mark-line"></i>
+                                        <span>Géneros</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item">
+                                    <a href="../Private/pedidos.html" class="nav__link">
+                                        <i class="ri-shopping-cart-line"></i>
+                                        <span>Pedidos</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item">
+                                    <a href="../Private/comentarios.html" class="nav__link">
+                                        <i class="ri-message-3-line"></i>
+                                        <span>Comentarios</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item">
+                                    <a href="../Private/usuarios.html" class="nav__link">
+                                        <i class="ri-user-line"></i>
+                                        <span>Usuarios</span>
+                                    </a>
+                                </li>
+                                <li class="nav__item nav__item-dropdown">
+                                    <a href="#" class="nav__link nav__link-dropdown">
+                                        <i class="ri-book-3-line"></i>
+                                        <span>Más...</span>
+                                        <i class="ri-arrow-down-s-line dropdown-icon"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="../Private/autores.html">Autores</a></li>
+                                        <li><a href="../Private/clasificacion.html">Clasificaciones</a></li>
+                                        <li><a href="../Private/editoriales.html">Editoriales</a></li>
+                                    </ul>
+                                </li>
+                                <i class="ri-user-line login-button" id="login-button"></i>
+                                <i class="ri-moon-line change-theme" id="theme-button"></i>
+                            </ul>
+                        </div>
+                    </nav>
+                `;
+                document.body.insertBefore(header, document.body.firstChild);
 
-// Agrega el contenido del header
-header.innerHTML = `
-    <nav class="nav container">
-        <a href="#" class="nav__logo">
-            <i class="ri-book-3-line"></i> Power Letters
-        </a>
-        <div class="nav__menu">
-            <ul class="nav__list">
-                <li class="nav__item">
-                    <a href="../Private/inicio_admin.html" class="nav__link active-link">
-                        <i class="ri-home-line"></i>
-                        <span>Inicio</span>
-                    </a>
-                </li>
-                <li class="nav__item">
-                    <a href="../Private/libros.html"  class="nav__link">
-                        <i class="ri-book-3-line"></i>
-                        <span>Libros</span>
-                    </a>
-                </li>
-                <li class="nav__item">
-                    <a href="../Private/generos.html" class="nav__link">
-                    <i class="ri-book-mark-line"></i>
-                        <span>Géneros</span>
-                    </a>
-                </li>
-               
-                <li class="nav__item">
-                    <a href="../Private/pedidos.html"  class="nav__link">
-                    <i class="ri-shopping-cart-line"></i>
-                        <span>Pedidos</span>
-                    </a>
-                </li>
-                <li class="nav__item">
-                    <a href="../Private/comentarios.html"  class="nav__link">
-                        <i class="ri-message-3-line"></i>
-                        <span>Comentarios</span>
-                    </a>
-                </li>
+                // Se agrega el pie de la página web después del contenido principal.
+                // Aquí puedes agregar el código HTML del pie de página.
 
-                <li class="nav__item">
-                <a href="../Private/usuarios.html"  class="nav__link">
-                <i class="ri-user-line"></i>
-                    <span>Usuarios</span>
-                </a>
-            </li>
+                // Añadir funcionalidad para el menú desplegable
+                document.addEventListener("DOMContentLoaded", function () {
+                    const dropdownLinks = document.querySelectorAll('.nav__link-dropdown');
+                    dropdownLinks.forEach(link => {
+                        link.addEventListener('click', function (e) {
+                            e.preventDefault();
+                            const dropdownMenu = this.nextElementSibling;
+                            dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+                        });
+                    });
+                });
+            } else {
+                sweetAlert(3, DATA.error, false, 'index.html');
+            }
+        } else {
+            // Redirigir a la página de inicio de sesión si no está autenticado
+            location.href = 'index.html';
+        }
+    } catch (error) {
+        console.error('Error loading template:', error);
+    }
+};
 
-                <li class="nav__item nav__item-dropdown">
-                    <a href="#" class="nav__link nav__link-dropdown">
-                        <i class="ri-book-3-line"></i>
-                        <span>Más...</span>
-                        <i class="ri-arrow-down-s-line dropdown-icon"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                    <li><a href="../Private/autores.html">Autores</a></li>
-                     <li><a href="../Private/clasificacion.html">Clasificaciones</a></li>
-                     <li><a href="../Private/editoriales.html">Editoriales</a></li>
-                    </ul>
-                </li>
-                
-                <!-- login button  -->
-
-                <i class="ri-user-line login-button" id="login-button"></i>
-    
-                  <!-- theame button  -->
-    
-                  <i class="ri-moon-line change-theme" id="theme-button"></i>
-            </ul>
-        </div>
-    </nav>
-`;
-
-
-
-
-// Inserta el header al principio del body
-document.body.insertBefore(header, document.body.firstChild);
-
-
-// Función para mostrar u ocultar el menú desplegable al hacer clic en el enlace "Autores"
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownLinks = document.querySelectorAll('.nav__link-dropdown');
-
-    dropdownLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const dropdownMenu = this.nextElementSibling;
-            dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-        });
-    });
-});
-
+// Llamar a la función para cargar la plantilla cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', loadTemplate);
