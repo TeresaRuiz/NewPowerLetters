cc00bc6860ab9bdd8638fd410e4b3d5a7b7d10ac
Teresa <20220651@ricaldone.edu.sv>
2024-05-28 13:31:19 -0600

    Añadiendo icono de carrito al menú

--- resources/js/header.js
+++ resources/js/header.js
@@ -61,6 +61,10 @@ header.innerHTML = `
 
             <i class="ri-user-line login-button" id="login-button"></i>
 
+            <!-- Carrito button  -->
+               
+            <i class="ri-shopping-cart-fill carrito-button" id="carrito-button"></i>
+
               <!-- theame button  -->
 
               <i class="ri-moon-line change-theme" id="theme-button"></i>
