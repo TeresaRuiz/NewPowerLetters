0787c7c62b5194a5038f1bc288fcae416189255d
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-06-03 11:13:19 -0600

    cambiar todo lo de usuario p

--- controllers/public/usuario_p.js
+++ controllers/public/usuario_p.js
@@ -1,118 +1,42 @@
-// Constante para completar la ruta de la API.
-const USUARIO_API = 'services/public/usuario.php';
-// Constantes para establecer los elementos del formulario de guardar.
-const SAVE_FORM = document.getElementById('signupForm'),
-    ID_USUARIO = document.getElementById('id_usuario'),
-    NOMBRE_CLIENTE = document.getElementById('nombreCliente'),
-    USUARIO_CLIENTE = document.getElementById('usuarioCliente'),
-    CORREO_CLIENTE = document.getElementById('correoCliente'),
-    DIRECCION_CLIENTE = document.getElementById('direccionCliente'),
-    TELEFONO_CLIENTE = document.getElementById('telefonoCliente');
-    CLAVE_CLIENTE = document.getElementById('claveAdministrador'),
-    CONFIRMAR_CLAVE = document.getElementById('confirmarClave');
+// Constante para establecer el formulario de registrar cliente.
+const USER_API = 'services/public/usuario.php';
+const SIGNUP_FORM = document.getElementById('signupForm');
 
 
-
-
-// Método del evento para cuando se envía el formulario de guardar.
-SAVE_FORM.addEventListener('submit', async (event) => {
+// Método del evento para cuando se envía el formulario de registrar cliente.
+SIGNUP_FORM.addEventListener('submit', async (event) => {
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
-
     // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(SAVE_FORM);
-    // Petición para guardar los datos del formulario.
-    try {
-        const DATA = await fetchData(USUARIO_API, 'signUp', FORM);
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-        if (DATA.status) {
-            // Se cierra la caja de diálogo.
-            SAVE_MODAL.hide();
-            // Se muestra un mensaje de éxito.
-            sweetAlert(1, DATA.message, true);
-            // Se carga nuevamente la tabla para visualizar los cambios.
-            fillTable();
-        } else {
-            sweetAlert(2, DATA.error, false);
-        }
-    } catch (e) {
-        console.log(e)
+    const FORM = new FormData(SIGNUP_FORM);
+    // Petición para registrar un cliente.
+    const DATA = await fetchData(USER_API, 'signUp', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'index.html');
+    } else if (DATA.recaptcha) {
+        sweetAlert(2, DATA.error, false, 'registro_c.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
+        // Se genera un nuevo token cuando ocurre un problema.
+        reCAPTCHA();
     }
 });
 
-
 /*
-*   Función para preparar el formulario al momento de insertar un registro.
+*   Función para obtener un token del reCAPTCHA y asignarlo al formulario.
 *   Parámetros: ninguno.
 *   Retorno: ninguno.
 */
-const openCreate = () => {
-    // Se muestra la caja de diálogo con su título.
-    SAVE_MODAL.show();
-    MODAL_TITLE.textContent = 'Crear usuario';
-    // Se prepara el formulario.
-    SAVE_FORM.reset();
-    USUARIO_CLIENTE.disabled = false;
-    CLAVE_CLIENTE.disabled = false;
-    CONFIRMAR_CLAVE.disabled = false;
-}
-
-/*
-*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
-const openUpdate = async (id) => {
-    // Se define una constante tipo objeto con los datos del registro seleccionado.
-    const FORM = new FormData();
-    FORM.append('id_usuario', id);
-    // Petición para obtener los datos del registro solicitado.
-    const DATA = await fetchData(ADMINISTRADOR_API, 'readOne', FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (DATA.status) {
-        // Se muestra la caja de diálogo con su título.
-        SAVE_MODAL.show();
-        MODAL_TITLE.textContent = 'Actualizar usuario del cliente';
-        // Se prepara el formulario.
-        SAVE_FORM.reset();
-        USUARIO_CLIENTE.disabled = true;
-        CLAVE_CLIENTE.disabled = true;
-        CONFIRMAR_CLAVE.disabled = true;
-        // Se inicializan los campos con los datos.
-        const ROW = DATA.dataset;
-        ID_USUARIO.value = ROW.id_usuario;
-        NOMBRE_CLIENTE.value = ROW.nombre_administrador;
-        DIRECCION_CLIENTE.value = ROW.direccion_cliente;
-        CORREO_CLIENTE.value = ROW.correo_cliente;
-        USUARIO_CLIENTE.value = ROW.usuario_cliente;
-    } else {
-        sweetAlert(2, DATA.error, false);
-    }
-}
-
-/*
-*   Función asíncrona para eliminar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
-const openDelete = async (id) => {
-    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar el usuario de forma permanente?');
-    // Se verifica la respuesta del mensaje.
-    if (RESPONSE) {
-        // Se define una constante tipo objeto con los datos del registro seleccionado.
-        const FORM = new FormData();
-        FORM.append('id_usuario', id);
-        // Petición para eliminar el registro seleccionado.
-        const DATA = await fetchData(ADMINISTRADOR_API, 'deleteRow', FORM);
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-        if (DATA.status) {
-            // Se muestra un mensaje de éxito.
-            await sweetAlert(1, DATA.message, true);
-            // Se carga nuevamente la tabla para visualizar los cambios.
-            fillTable();
-        } else {
-            sweetAlert(2, DATA.error, false);
-        }
-    }
+function reCAPTCHA() {
+    // Método para generar el token del reCAPTCHA.
+    grecaptcha.ready(() => {
+        // Constante para establecer la llave pública del reCAPTCHA.
+        const PUBLIC_KEY = '6LdBzLQUAAAAAJvH-aCUUJgliLOjLcmrHN06RFXT';
+        // Se obtiene un token para la página web mediante la llave pública.
+        grecaptcha.execute(PUBLIC_KEY, { action: 'homepage' }).then((token) => {
+            // Se asigna el valor del token al campo oculto del formulario
+            document.getElementById('gRecaptchaResponse').value = token;
+        });
+    });
 }
