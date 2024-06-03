0663f935b11523f73cfb0a4df7b1d75428adb1d1
20220651 <20220651@ricaldone.edu.sv>
2024-06-03 10:04:38 -0600

    añadiendo modal de cantidad y js para carrito

---
+++ controllers/public/carrito.js
@@ -0,0 +1,4 @@
+// Constante para completar la ruta de la API.
+const PEDIDO_API = 'services/public/pedido.php';
+// Constante para establecer el cuerpo de la tabla.
+const TABLE_BODY = document.getElementById('tableBody');
