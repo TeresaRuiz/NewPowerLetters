dcf89a3105e3b87c6b39e00c58ba91230ec4563f
TeresaRuiz <107508345+TeresaRuiz@users.noreply.github.com>
2024-06-04 08:07:54 -0600

    agregando id_usuario libros_descuentos.php

--- api/services/public/libros_descuentos.php
+++ api/services/public/libros_descuentos.php
@@ -18,7 +18,8 @@ if (isset($_GET['action'])) {
         'exception' => null, // Excepción del servidor de base de datos si es aplicable.
         'fileStatus' => null
     ); // Estado de archivo (si es necesario para alguna operación).
-
+// Verificar si el usuario tiene una sesión iniciada como administrador.
+    if (isset($_SESSION['idUsuario'])) {
     // Usar un 'switch' para manejar la acción específica solicitada por el usuario.
     switch ($_GET['action']) {
         case 'searchRows':
