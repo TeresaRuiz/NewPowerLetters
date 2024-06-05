8ff5694ffd2209c8fdde41931360e2d67ce17e2e
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-06-05 09:42:20 -0600

    modo oscuro funciona

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -191,4 +191,31 @@ const loadTemplate = async () => {
         });
     }
 
+     // Configurar el cambio de tema
+     const themeButton = document.getElementById('theme-button');
+     const darkTheme = 'dark-theme';
+     const iconTheme = 'ri-sun-line';
+
+     // Obtiene el tema e icono seleccionados previamente
+     const selectedTheme = localStorage.getItem('selected-theme');
+     const selectedIcon = localStorage.getItem('selected-icon');
+
+     // Obtiene el tema e icono actuales
+     const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+     const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+     // Valida si el usuario eligió un tema previamente
+     if (selectedTheme) {
+         document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+         themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+     }
+
+     // Activa/desactiva el tema con el botón
+     themeButton.addEventListener('click', () => {
+         document.body.classList.toggle(darkTheme);
+         themeButton.classList.toggle(iconTheme);
+         localStorage.setItem('selected-theme', getCurrentTheme());
+         localStorage.setItem('selected-icon', getCurrentIcon());
+     });
+
 };
