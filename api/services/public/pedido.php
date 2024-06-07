543fd0cba9fb3161fb23126e492af23d1ed425a0
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-06-07 02:18:23 -0600

    intentando arreglar carrito

--- api/services/public/pedido.php
+++ api/services/public/pedido.php
@@ -11,6 +11,7 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'session' => 0, 'message' => null, 'error' => null, 'exception' => null, 'dataset' => null);
     // Se verifica si existe una sesión iniciada como cliente para realizar las acciones correspondientes.
+    
     if (isset($_SESSION['idUsuario'])) {
         $result['session'] = 1;
         // Se compara la acción a realizar cuando un cliente ha iniciado sesión.
